58d827c654ebb1c7a963713947fa6f2cac5661fd
tim <timothe.vannier@etu.iut-tlse3>
2022-12-19 20:30:05 +0100

    Added body on sensor wip : randomize function to upgrade wip : add config file

--- src/aeroport/pressureCaptor/pressureCaptor.go
+++ src/aeroport/pressureCaptor/pressureCaptor.go
@@ -1,1 +1,99 @@
-package pressureCaptor
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
+	"math/rand"
+	"time"
+)
+
+// A utiliser pour décoder le json par le sub aussi
+// Contient les différents champs à récupérer
+type captorDatas struct {
+	IdSensor     int     //id du capteur
+	IdAeroport   string  //id aéroport (code "IATA" 3 caractères)
+	NatureMesure string  //Nature mesure ("Temperature","Atmospheric pressure", "Wind speed")
+	Valeur       float32 //Valeur de la mesure (numérique)
+	DateHeureMes string  //Date et heure de la mesure (timestamp : YYYY-MM-DD-hh-mm-ss)
+}
+
+// Créer client
+func createClientOptions(brokerUrl string, clientId string) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(brokerUrl)
+	opts.SetClientID(clientId)
+	return opts
+}
+
+// Connection au broker
+func connect(brokerURI string, clientId string) mqtt.Client {
+	fmt.Println("Trying to connect (" + brokerURI + ", " + clientId + ")...")
+	opts := createClientOptions(brokerURI, clientId)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+// fonction random
+func generateDatas(codeAeroport string) captorDatas {
+	t := time.Now()
+	var data = captorDatas{
+		IdSensor:     2,
+		IdAeroport:   codeAeroport,
+		NatureMesure: "Atmospheric pressure",
+		DateHeureMes: t.Format("2006-01-02-15-04-05"),
+	}
+	data.Valeur = randomPressure()
+	return data
+}
+
+// TODO: à améliorer pour prendre en compte les anciennes valeurs de chaque airport pour pas trop faire varier les résultats
+func randomPressure() float32 {
+	var tmp float32
+	for x := 0; tmp >= 1013 && tmp <= 1028; x++ {
+		tmp = float32(rand.Intn(102) * 10)
+	}
+	return tmp
+}
+
+// Fonction qui génère une string au format JSON à partir d'une structure captorDatas
+func encodeJson(datas captorDatas) string {
+	empData := &datas
+	e, err := json.Marshal(empData)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(e)
+}
+
+// fonction main
+func main() {
+
+	var codesAeroport = []string{"CDG", "BOD", "CFE", "DIJ", "GNB", "JCA", "LRH", "NTE"}
+
+	//TODO: à changer lors du fichier config
+	urlBroker := "tcp://51.210.45.234:1883"
+	clientId := "PressureCaptor"
+
+	client_pub := connect(urlBroker, clientId)
+
+	for _, airportCode := range codesAeroport {
+		data := generateDatas(airportCode)
+		jsonDatas := encodeJson(data)
+
+		client_pub.Publish(urlBroker+"/airport/"+"airportCode"+"/sensor/pressure", 0, false, jsonDatas) //topic à changer
+		time.Sleep(3 * time.Minute)
+		if !client_pub.IsConnected() {
+			break
+		}
+	}
+
+}
